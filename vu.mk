ea2c73ab76a9cc13526ef191d8cbea5253c56a83
puk3n <slax775@gmail.com>
2016-03-18 12:06:35 -0400

    **make it vu

--- vu.mk
+++ vu.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := i9300
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
+# Inherit some common vu stuff.
 $(call inherit-product, vendor/vu/config/common_full_phone.mk)
 
 # Inherit device configuration
